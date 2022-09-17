094d3c62ee0b0675f11afe2d2fd59f856ec97613
yildiz-harun <yildizharun@outlook.com>
2022-09-17 20:35:17 +0300

    Update: navbar component added, styling update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,19 +6,23 @@ import App from './App';
 import { Register } from './pages/Register';
 import { Nopage } from './pages/Nopage';
 import { Login } from './pages/Login';
+import { Navbar } from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <div className='flex justify-center w-screen'>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<Nopage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className='max-w-4xl mx-auto h-screen'>
+      <BrowserRouter>
+        <Navbar />
+        <div className='flex justify-center'>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<Nopage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   </React.StrictMode >
 );
