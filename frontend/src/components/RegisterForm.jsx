35c50526160198362a6e5122b9c9cb1f07202d6a
yildiz-harun <yildizharun@outlook.com>
2022-09-17 20:36:16 +0300

    Update: Basic register form component added

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,18 +1,31 @@
+import { Link } from 'react-router-dom'
+
 export const RegisterForm = () => {
   return (
-    <form className="flex flex-col p-6 w-11/12 sm:w-3/5">
+    <form className="flex flex-col p-6 w-11/12 shadow-md rounded-md max-w-md">
+      <label>Username: </label>
       <input
-        placeholder="username"
-        className="border border-gray-900 m-1 p-1.5"
+        placeholder="Enter your username"
+        className="border rounded-md border-gray-900 my-3 p-3"
         type="text"
       />
+      <label>Password: </label>
       <input
-        placeholder="password"
-        className="border border-gray-900 m-1 p-1.5"
-        type="text"
+        placeholder="Enter your password"
+        className="border rounded-md border-gray-900 my-3 p-3"
+        type="password"
       />
-      <button className="border border-gray-900 m-1 p-1.5">
-        Register
+      <label htmlFor="">
+        Have an account? Click here to:{' '}
+        <Link
+          to={'/login'}
+          className="cursor-pointer underline text-blue-700"
+        >
+          Login
+        </Link>
+      </label>
+      <button className="border rounded-md bg-green-500 border-none hover:bg-green-600 my-3 p-3 text-white">
+        <Link to={'/'}>Register</Link>
       </button>
     </form>
   )
