b758e56aea2bbb4422e350be89aff2b6eca1de19
yildiz-harun <yildizharun@outlook.com>
2022-09-17 20:34:11 +0300

    Update: logo added, link (react-router-dom) added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom'
+import logo from '../../src/logo.svg'
+
 export const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <Link
+      to={'/'}
+      className="flex justify-start items-center h-12 mb-36 md:mb-36"
+    >
+      <img
+        className="w-12 h-12"
+        src={logo}
+        alt="logo"
+      />
+      <h1 className="text-lg">Social Media</h1>
+    </Link>
+  )
 }
