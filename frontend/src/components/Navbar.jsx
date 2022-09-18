0b1e572b99d2c3362461412000fc76236c401072
yildiz-harun <yildizharun@outlook.com>
2022-09-18 22:35:05 +0300

    Update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,53 @@
+import {
+  useSelector,
+  useDispatch,
+} from 'react-redux'
+import {
+  addUser,
+  removeUser,
+} from '../features/user/userSlice'
+
 import { Link } from 'react-router-dom'
 import logo from '../../src/logo.svg'
 
 export const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const user = useSelector(
+    (state) => state.user.value
+  )
+
   return (
-    <Link
-      to={'/'}
-      className="flex justify-start items-center h-12 mb-36 md:mb-0"
-    >
-      <img
-        className="w-12 h-12"
-        src={logo}
-        alt="logo"
-      />
-      <h1 className="text-lg">Social Media</h1>
-    </Link>
+    <div className=" bg-zinc-50 flex justify-between items-center h-12 mb-36 md:mb-0">
+      <div className="flex items-center">
+        <Link to={'/'}>
+          <img
+            className="w-12 h-12"
+            src={logo}
+            alt="logo"
+          />
+        </Link>
+        <Link to={'/'}>
+          <h1 className="text-lg">
+            Social Media
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center mr-2">
+        <div className="flex items-center mr-2">
+          {user && user.username}
+        </div>
+        {user && (
+          <button
+            onClick={() => {
+              dispatch(removeUser())
+              document.cookie = null
+            }}
+          >
+            Log out
+          </button>
+        )}
+      </div>
+    </div>
   )
 }
