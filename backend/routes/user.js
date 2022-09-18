c74e412006087aa90d4fbee3aa50d440b8d68bb2
yildiz-harun <yildizharun@outlook.com>
2022-09-18 22:10:01 +0300

    Update: login router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const userRouter = express.Router()
 
-const { register } = require("../controllers/user")
+const { register,login } = require("../controllers/user")
 
 userRouter.post("/register", register)
+userRouter.post("/login", login)
 
 module.exports = { userRouter }
