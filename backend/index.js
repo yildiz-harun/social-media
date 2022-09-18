25732906cddf6330fb55569366a71e687739a328
yildiz-harun <yildizharun@outlook.com>
2022-09-18 15:52:35 +0300

    Update: npm cors added, useless app.get removed

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
 require("dotenv").config({ path: './config/.env' })
+const cors = require("cors")
 
 const { connectDB } = require("./config/db")//function for connection to the database
 const express = require("express")
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.listen("5000", () => {
     console.log("Server started listening on port: 5000");
     connectDB()
-})
-
-app.get("/", (req, res) => {
-    res.send("/")
 })
 
 const { userRouter } = require("./routes/user")
