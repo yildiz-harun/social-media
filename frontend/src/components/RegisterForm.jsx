4f9f755e55675760118d3386f493180c430defac
yildiz-harun <yildizharun@outlook.com>
2022-09-18 15:56:40 +0300

    Update: input handlers

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,16 +1,50 @@
+import { useState } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 export const RegisterForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await axios.post(
+      'http://localhost:5000/api/user/register',
+      { username: username, password: password }
+    )
+
+    // // store object to the cookies
+    // let user = {
+    //   username: response.data.username,
+    //   token: response.data.token,
+    // }
+    document.cookie =
+      'user=' + JSON.stringify(response.data)
+    //save the token
+  }
+
   return (
-    <form className="flex flex-col p-2 w-11/12 shadow-md rounded-md max-w-md">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col p-2 w-11/12 shadow-md rounded-md max-w-md"
+    >
       <label>Username: </label>
-      <input  
+      <input
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}
         placeholder="Enter your username"
         className="border rounded-md border-gray-900 my-3 p-3"
         type="text"
       />
       <label>Password: </label>
       <input
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
         placeholder="Enter your password"
         className="border rounded-md border-gray-900 my-3 p-3"
         type="password"
@@ -25,7 +59,7 @@ export const RegisterForm = () => {
         </Link>
       </label>
       <button className="border rounded-md bg-green-500 border-none hover:bg-green-600 my-3 p-3 text-white">
-        <Link to={'/'}>Register</Link>
+        Register
       </button>
     </form>
   )
