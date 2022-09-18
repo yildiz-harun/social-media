d9e7b1d4869335f12800c3f94bc243f312dd4838
yildiz-harun <yildizharun@outlook.com>
2022-09-18 22:11:36 +0300

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";//react router package
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-import { Register } from './pages/Register';
-import { Nopage } from './pages/Nopage';
-import { Login } from './pages/Login';
-import { Navbar } from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='max-w-4xl mx-auto h-screen'>
-      <BrowserRouter>
-        <Navbar />
-        <div className='flex justify-center'>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<Nopage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode >
 );
