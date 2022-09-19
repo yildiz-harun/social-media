0c39586c54a7a250b34ba2e1e2676beba931380e
yildiz-harun <yildizharun@outlook.com>
2022-09-19 22:58:55 +0300

    Update: postReducer added

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../features/user/userSlice'
+import postReducer from '../features/posts/postSlice'
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        posts: postReducer,
     },
 })
