9f23b2ffa8e00138cda211d28293901ee7ee56b8
yildiz-harun <yildizharun@outlook.com>
2022-09-19 22:59:59 +0300

    Update: onSubmit and onChange funcs. added

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -1,12 +1,27 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 function PostForm() {
+  const [post, setPost] = useState('')
+
   return (
     <form
       className="border border-t-0 p-1"
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault()
+        const response = await axios.post(
+          'http://localhost:5000/api/posts',
+          {
+            value: post,
+          }
+        )
       }}
     >
       <textarea
+        onChange={(e) => {
+          setPost(e.target.value)
+        }}
+        value={post}
         className="w-full outline-none text-sm overflow-hidden"
         type="text"
         placeholder="What' s happening?"
