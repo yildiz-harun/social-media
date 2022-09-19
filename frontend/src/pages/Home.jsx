416f608f34b1c0a23a4bf515cfa2c878570c43c2
yildiz-harun <yildizharun@outlook.com>
2022-09-19 23:00:34 +0300

    Update: redux logic added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import Post from '../components/Post'
 import PostForm from '../components/PostForm'
+import {
+  useSelector,
+  useDispatch,
+} from 'react-redux'
+import { setPost } from '../features/posts/postSlice'
 
 function Home() {
+  const dispatch = useDispatch()
+
+  const posts = useSelector(
+    (state) => state.posts.value
+  )
+
+  useEffect(() => {
+    const foo = async () => {
+      const response = await axios.get(
+        'http://localhost:5000/api/posts'
+      )
+      console.log(response.data)
+      dispatch(setPost(response.data))
+      console.log(posts[0])
+    }
+    foo()
+  }, [])
+
   return (
     <div className="w-screen min-h-screen">
       <div className="border mt-2 p-1">Home</div>
       <PostForm />
-      <Post />
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post._id}
+            value={post.value}
+          />
+        )
+      })}
     </div>
   )
 }
