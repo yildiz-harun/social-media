995cbda14faf1772d38659f717db0c17f9705736
yildiz-harun <yildizharun@outlook.com>
2022-09-21 20:09:12 +0300

    Update: auth controller added

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,6 +3,9 @@ const express = require("express")
 const postRouter = express.Router()
 
 const { getPosts, getPost, createPost, updatePost, deletePost } = require("../controllers/post")
+const { controlAuth } = require("../middleware/authController")
+
+postRouter.use(controlAuth)
 
 postRouter.get("/", getPosts)
 postRouter.get("/:id", getPost)
