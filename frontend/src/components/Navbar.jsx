870c6099317e884891693abdc66ae61b94638e84
yildiz-harun <yildizharun@outlook.com>
2022-09-21 20:10:13 +0300

    Refactor

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,53 +1,37 @@
-import {
-  useSelector,
-  useDispatch,
-} from 'react-redux'
-import {
-  addUser,
-  removeUser,
-} from '../features/user/userSlice'
+import { useSelector, useDispatch } from "react-redux"
+import { addUser, removeUser } from "../features/user/userSlice"
 
-import { Link } from 'react-router-dom'
-import logo from '../../src/logo.svg'
+import { Link } from "react-router-dom"
+import logo from "../../src/logo.svg"
 
 export const Navbar = () => {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-  const user = useSelector(
-    (state) => state.user.value
-  )
+	const user = useSelector((state) => state.user.value)
 
-  return (
-    <div className=" bg-zinc-50 flex justify-between items-center h-12 ">
-      <div className="flex items-center">
-        <Link to={'/'}>
-          <img
-            className="w-12 h-12"
-            src={logo}
-            alt="logo"
-          />
-        </Link>
-        <Link to={'/'}>
-          <h1 className="text-lg">
-            Lorem
-          </h1>
-        </Link>
-      </div>
-      <div className="flex items-center mr-2">
-        <div className="flex items-center mr-2">
-          {user && user.username}
-        </div>
-        {user && (
-          <button
-            onClick={() => {
-              dispatch(removeUser())
-              document.cookie = null
-            }}
-          >
-            Log out
-          </button>
-        )}
-      </div>
-    </div>
-  )
+	return (
+		<div className=" bg-zinc-50 flex justify-between items-center h-12 ">
+			<div className="flex items-center">
+				<Link to={"/"}>
+					<img className="w-12 h-12" src={logo} alt="logo" />
+				</Link>
+				<Link to={"/"}>
+					<h1 className="text-lg">Lorem</h1>
+				</Link>
+			</div>
+			<div className="flex items-center mr-2">
+				<div className="flex items-center mr-2">{user && user.username}</div>
+				{user && (
+					<button
+						onClick={() => {
+							dispatch(removeUser())
+							document.cookie = null
+						}}
+					>
+						Log out
+					</button>
+				)}
+			</div>
+		</div>
+	)
 }
