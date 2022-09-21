86d6382a61f9c95db0f43cb499daebc98f85405b
yildiz-harun <yildizharun@outlook.com>
2022-09-21 20:11:02 +0300

    Update: save button and update from added

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,17 +1,46 @@
-function Post({ value }) {
-  return (
-    <div className="border text-sm p-1 mt-1">
-      <p>{value}</p>
-      <div className="flex justify-end">
-        <button className="bg-blue-500 text-white rounded-md text-[10px] mr-1 p-[2px] w-12 h-6">
-          Update
-        </button>
-        <button className="bg-blue-500 text-white rounded-md text-[10px] p-[2px] w-12 h-6">
-          Delete
-        </button>
-      </div>
-    </div>
-  )
+import { useState } from "react"
+import axios from "axios"
+import DeleteButton from "./post_components/DeleteButton"
+import UpdateButton from "./post_components/UpdateButton"
+import PostContent from "./post_components/PostContent"
+import SaveButton from "./post_components/SaveButton"
+import UpdateForm from "./post_components/UpdateForm"
+
+function Post({ post }) {
+	const [isUpdating, setIsUpdating] = useState(false)
+
+	const [newPost, setNewPost] = useState("")
+
+	return (
+		<div className="border text-sm p-1 mt-1">
+			{!isUpdating && (
+				<div>
+					<div>
+						<PostContent post={post} />
+					</div>
+					<div className="flex justify-end">
+						<UpdateButton
+							post={post}
+							setIsUpdating={setIsUpdating}
+							setNewPost={setNewPost}
+						/>
+						<DeleteButton post={post} />
+					</div>
+				</div>
+			)}
+
+			{isUpdating && (
+				<div>
+					<UpdateForm newPost={newPost} setNewPost={setNewPost} />
+					<SaveButton
+						setIsUpdating={setIsUpdating}
+						newPost={newPost}
+						post={post}
+					/>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Post
