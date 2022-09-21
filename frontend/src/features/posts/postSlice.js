39b43d1fbe30a5e94707deeac4f75b5a1018e199
yildiz-harun <yildizharun@outlook.com>
2022-09-21 20:13:24 +0300

    Update: add post, delete post, update post added

--- frontend/src/features/posts/postSlice.js
+++ frontend/src/features/posts/postSlice.js
@@ -16,10 +16,37 @@ export const postSlice = createSlice({
             // immutable state based off those changes
             //   state.value += 1
         },
+        addPost: (state, action) => {
+            state.value = [action.payload, ...state.value]
+        },
+        deletePost: (state, action) => {
+            //state.value = state.value.filter((post) => { return post._id !== action.payload._id })
+            state.value = state.value.filter((post) => {
+                // if (post._id === action.payload._id) {
+                //     console.log(post._id);
+                // }
+                return post._id !== action.payload._id
+            })
+        },
+        updatePost: (state, action) => {
+            state.value = state.value.map((post) => {
+                if (post._id === action.payload._id) {
+                    return {
+                        value: action.payload.value, _id: action.payload._id,
+                        createdAt: post.createdAt, updatedAt: post.updatedAt, __v: post.__v
+                    }
+                    //return { value: action.payload.value, _id: post._id }
+                }
+                else {
+                    return post
+                }
+            })
+            //state.todos = state.todos.map((todo) => { ((todo.id === action.payload.id) ? return { todo.content : "111111111", todo.id: todo.id } : return todo))}
+        }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { setPost } = postSlice.actions
+export const { setPost, addPost, deletePost, updatePost } = postSlice.actions
 
 export default postSlice.reducer
