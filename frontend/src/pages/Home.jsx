0bbd373147bebee9fe533ad1b5968600065f53a2
yildiz-harun <yildizharun@outlook.com>
2022-09-21 20:13:43 +0300

    Refactor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,46 +1,35 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import Post from '../components/Post'
-import PostForm from '../components/PostForm'
-import {
-  useSelector,
-  useDispatch,
-} from 'react-redux'
-import { setPost } from '../features/posts/postSlice'
+import React, { useEffect } from "react"
+import axios from "axios"
+import Post from "../components/Post"
+import PostForm from "../components/PostForm"
+import { useSelector, useDispatch } from "react-redux"
+import { setPost } from "../features/posts/postSlice"
 
 function Home() {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
+	const posts = useSelector((state) => state.posts.value)
+	const user = useSelector((state) => state.user.value)
 
-  const posts = useSelector(
-    (state) => state.posts.value
-  )
+	console.log(user.token)
+	useEffect(() => {
+		const foo = async () => {
+			const response = await axios.get("http://localhost:5000/api/posts", {
+				headers: { Authorization: "Bearer " + user.token },
+			})
+			dispatch(setPost(response.data))
+		}
+		foo()
+	}, [])
 
-  useEffect(() => {
-    const foo = async () => {
-      const response = await axios.get(
-        'http://localhost:5000/api/posts'
-      )
-      console.log(response.data)
-      dispatch(setPost(response.data))
-      console.log(posts[0])
-    }
-    foo()
-  }, [])
-
-  return (
-    <div className="w-screen min-h-screen">
-      <div className="border mt-2 p-1">Home</div>
-      <PostForm />
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post._id}
-            value={post.value}
-          />
-        )
-      })}
-    </div>
-  )
+	return (
+		<div className="w-screen min-h-screen">
+			<div className="border mt-2 p-1">Home</div>
+			<PostForm />
+			{posts.map((post) => {
+				return <Post key={post._id} post={post} />
+			})}
+		</div>
+	)
 }
 
 export default Home
